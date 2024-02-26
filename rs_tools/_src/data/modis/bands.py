0668dea152ae90e05dda71379a3989eef64a4e9c
jejjohnson <emanjohnson91@gmail.com>
2024-02-26 11:21:39 +0100

    Test geoprocessing pipeline for MODIS.

--- rs_tools/_src/data/modis/bands.py
+++ rs_tools/_src/data/modis/bands.py
@@ -4,3 +4,8 @@ MODIS_VARIABLES = {
         "EV_1KM_RefSB": [8,9,10,11,12,"13lo","13hi","14lo","14hi",15,16,17,18,19,26],
         "EV_1KM_Emissive": [20,21,22,23,24,25,27,28,29,30,31,32,33,34,35,36],
 }
+
+
+MODIS_CHANNEL_NUMBERS = [
+    str(i) for i in range(1, 39)
+] + ['13lo', '14lo', '13hi', '14hi']
