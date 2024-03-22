7fa21445b0f701782f826d452165c8f8fbb91d1f
annajungbluth <anna.jungbluth1993@gmail.com>
2024-03-22 11:31:42 +0000

    continued hydra integration and finished download components
    Co-authored-by: Lilli Freischem <lillif@users.noreply.github.com>
    Co-authored-by: JUAN EMMANUEL JOHNSON <juanjohn@ucm.es>

--- rs_tools/__main__.py
+++ rs_tools/__main__.py
@@ -1,40 +1,39 @@
 import hydra
+from typing import Dict
 from loguru import logger
 from omegaconf import OmegaConf, DictConfig
 
 
-@hydra.main(config_path="../config", config_name="main", version_base="1.3")
+@hydra.main(config_path="../config/example/", config_name="main", version_base="1.3")
 def main(cfg):
-    lib = hydra.utils.instantiate(cfg...)
-
-    logger.debug(f"Choosing stage...")
+    # instantiate the config 
+    logger.debug(f"Choosing Stage...")
     if cfg.stage == "download":
-        for satellite in cfg.download.keys():
-            # instantiate download function
-            # pick parameters
-            # download
-        logger.debug(f"starting download script...")
+        logger.info(f"Instantiating Datamodule: {cfg.satellite.download['_target_']}")
+        download = hydra.utils.instantiate(cfg.satellite.download)
+        logger.info(f"Starting Download")
+        hydra.utils.call(download)
+
+    elif cfg.stage == "geoprocess":
+        logger.debug(f"starting geoprocessing script...")
         raise NotImplementedError()
+    
     elif cfg.stage == "preprocess":
         logger.debug(f"starting preprocessing script...")
         raise NotImplementedError()
-    # elif cfg.stage == "train":
-    #     logger.debug(f"starting training script...")
-    #     raise NotImplementedError()
-    # elif cfg.stage == "inference":
-    #     logger.debug(f"starting inference script...")
-    #     raise NotImplementedError()
-    # elif cfg.stage == "evaluation":
-    #     logger.debug(f"starting evaluation script...")
-    #     raise NotImplementedError()
+    
+    elif cfg.stage == "patch":
+        logger.debug(f"starting patching script...")
+        raise NotImplementedError()
+    
     else:
         raise ValueError(f"Unrecognized stage: {cfg.stage}")
 
-
-
-def main(config: DictConfig):
-    pass
-
 if __name__ == "__main__":
     main()
+
+""""
+EXAMPLES:
+python rs_tools satellite=goes
+"""
 
