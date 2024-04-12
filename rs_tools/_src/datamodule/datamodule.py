3513f3b09ad243b788efff9937089db29149e4ad
annajungbluth <anna.jungbluth1993@gmail.com>
2024-04-12 13:44:55 +0100

    started developing datamodule

--- rs_tools/_src/datamodule/datamodule.py
+++ rs_tools/_src/datamodule/datamodule.py
@@ -1,28 +1,36 @@
 from __future__ import annotations
 
 from lightning.pytorch import LightningDataModule
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, RandomSampler
 from rs_tools._src.datamodule.components.base import BaseDataset
+from rs_tools._src.utils.io import get_files
 
 class ITIDataModule(LightningDataModule):
     def __init__(
         self,
         datasets_spec,
-        split_file,
-        transforms=None,
-        batch_size: int = 16,
+        batch_size: int = 4,
+        iterations_per_epoch=1e4,
         num_workers: int = 1,
-        pin_memory: bool = False,
-        prefetch_factor: int = 4,
         train_shuffle=True,
     ):
         super().__init__()
-        self.save_hyperparameters(logger=False)
-        self.prefetch_factor = prefetch_factor
-        self.pin_memory = pin_memory
-        self.train_shuffle = (
-            train_shuffle
-        )
+        self.datasets_spec = datasets_spec
+        self.batch_size = batch_size
+        self.iterations_per_epoch = iterations_per_epoch
+        self.num_workers = num_workers
+        self.train_shuffle = train_shuffle
+
+        if not isinstance(datasets_spec, dict):
+            raise ValueError("datasets_spec must be a dictionary")
+        
+        datasets = datasets_spec.keys()
+        if len(datasets) != 2:
+            raise ValueError("datasets_spec must contain two datasets")
+        
+        # Get filenames from dataset specs
+        self.A_filenames = get_files(datasets_spec=datasets_spec[datasets[0]])
+        self.B_filenames = get_files(datasets_spec=datasets_spec[datasets[1]])
 
         # TODO: Add function to filter train/test/val split
         # Filter data for months
@@ -31,33 +39,45 @@ class ITIDataModule(LightningDataModule):
         # TODO: Calculate mean/std for training set
         # Go through all files and calculate mean/stdev
 
-        self.train_A = BaseDataset(...)
-        self.train_B = BaseDataset(...)
-        self.val_A = BaseDataset(...)
-        self.val_B = BaseDataset(...)
-
-    # def prepare_data(self):
-    #     self.train_dataset.prepare_data()
-    #     self.test_dataset.prepare_data()
-    #     self.val_dataset.prepare_data()
+        self.A_train_ds = BaseDataset(...)
+        self.B_train_ds = BaseDataset(...)
+        self.A_valid_ds = BaseDataset(...)
+        self.B_valid_ds = BaseDataset(...)
 
     def train_dataloader(self):
-        gen_A = DataLoader(dataset=self.train_A, ...)
-        gen_B = DataLoader(dataset=self.train_B, ...)
-        dis_A = DataLoader(dataset=self.train_A, ...)
-        dis_B = DataLoader(dataset=self.train_B, ...)
-        # return DataLoader(
-        #     dataset=self.train_dataset,
-        #     batch_size=self.hparams.batch_size,
-        #     num_workers=self.hparams.num_workers,
-        #     pin_memory=self.pin_memory,
-        #     shuffle=self.train_shuffle,
-        #     persistent_workers=True,
-        #     prefetch_factor=self.hparams.prefetch_factor,
-        # )
+        gen_A = DataLoader(dataset=self.A_train_ds,
+                           batch_size=self.batch_size,
+                           num_workers=self.num_workers,
+                           shuffle=self.train_shuffle,
+                           sampler=RandomSampler(self.A_train_ds, replacement=True, num_samples=self.iterations_per_epoch)
+        )
+        dis_A = DataLoader(dataset=self.A_train_ds,
+                            batch_size=self.batch_size,
+                            num_workers=self.num_workers,
+                            shuffle=self.train_shuffle,
+                            sampler=RandomSampler(self.A_train_ds, replacement=True, num_samples=self.iterations_per_epoch)
+          )
+        gen_B = DataLoader(dataset=self.B_train_ds,
+                           batch_size=self.batch_size,
+                           num_workers=self.num_workers,
+                           shuffle=self.train_shuffle,
+                           sampler=RandomSampler(self.B_train_ds, replacement=True, num_samples=self.iterations_per_epoch)
+        )
+        dis_B = DataLoader(dataset=self.B_train_ds,
+                            batch_size=self.batch_size,
+                            num_workers=self.num_workers,
+                            shuffle=self.train_shuffle,
+                            sampler=RandomSampler(self.B_train_ds, replacement=True, num_samples=self.iterations_per_epoch)
+          )
         return {"gen_A": gen_A, "dis_A": dis_A, "gen_B": gen_B, "dis_B": dis_B}
     
     def val_dataloader(self):
-            A = DataLoader(self.A_valid, ...)
-            B = DataLoader(self.B_valid, ...)
+            A = DataLoader(self.A_valid_ds, 
+                           batch_size=self.batch_size, 
+                           num_workers=self.num_workers,
+                           shuffle=False)
+            B = DataLoader(self.B_valid_ds, 
+                           batch_size=self.batch_size, 
+                           num_workers=self.num_workers,
+                           shuffle=False)
             return [A, B]
