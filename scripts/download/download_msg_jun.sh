a62157ce816986fae3d3c154e653d392463000c1
annajungbluth <anna.jungbluth1993@gmail.com>
2024-06-17 13:31:37 +0000

    added scripts

--- scripts/download/download_msg_jun.sh
+++ scripts/download/download_msg_jun.sh
@@ -4,8 +4,8 @@ set -o xtrace
 
 SAVE_DIR="/mnt/disks/data/msg/2010/"
 
-START_DATE="2010-04-01" # repeat download for cloudmask!!!
-END_DATE="2010-04-30"
+START_DATE="2010-06-01" # repeat download for cloudmask!!!
+END_DATE="2010-06-30"
 
 python rs_tools/_src/data/msg/downloader_msg_modis_overpass.py --save-dir $SAVE_DIR --start-date $START_DATE --end-date $END_DATE
 
